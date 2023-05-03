7b886e9d0ecacc159a393ed8c4b2ecd252bb6475
soubj <sobujahmed124@gmail.com>
2023-05-03 21:06:19 +0600

    password validation with js regular expression

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -6,6 +6,7 @@ const auth = getAuth(app);
 
 const Register = () => {
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleSubmit = (event) => {
     // event.preventDefault() will stop form tag from refreshing the page
@@ -14,12 +15,29 @@ const Register = () => {
     const email = event.target.email.value;
     const password = event.target.password.value;
     console.log(email, password);
+
+    setError("");
+    // js regular expression password validation
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("Please add at least one uppercase.");
+      return;
+    } else if (!/(?=.*\d)/.test(password)) {
+      setError("Please include at least one number.");
+      return;
+    } else if (password.length < 6) {
+      setError("password must contain at least 6 digit");
+      return;
+    }
+
     // create user in firebase
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
         setError("");
+        // resetting or clearing the input fields in the form
+        event.target.reset();
+        setSuccess("User has been created successfully.");
       })
       .catch((error) => {
         console.error(error.message);
@@ -62,6 +80,7 @@ const Register = () => {
         <input className="btn btn-primary" type="submit" value="Register" />
       </form>
       <p className="text-danger">{error}</p>
+      <p>{success}</p>
     </div>
   );
 };
