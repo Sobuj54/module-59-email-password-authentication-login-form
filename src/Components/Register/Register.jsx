8b3c14efc45ee91c4c3d4b84ea8115ce51ba1fb5
soubj <sobujahmed124@gmail.com>
2023-05-03 13:48:14 +0600

    used onSubmit, onBlur, onChange as event handler

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    // event.preventDefault() will stop form tag from refreshing the page
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordOnBlur = (event) => {
+    // console.log(event.target.value);
+  };
+
   return (
     <div>
       <h2>Register</h2>
-      <form>
-        <input type="email" name="email" id="email" placeholder="email" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+        />
         <br />
         <input
+          onBlur={handlePasswordOnBlur}
           type="password"
           name="password"
           id="password"
