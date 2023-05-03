dba1ac4ba0c768ed3ae812ea8de29a6a584002f8
soubj <sobujahmed124@gmail.com>
2023-05-03 21:26:29 +0600

    log in form recap getting email and password

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -1,9 +1,56 @@
 import React from "react";
 
 const LogIn = () => {
+  const handleLogIn = (event) => {
+    // stopping form tag from reloading the page
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h2>log in</h2>
+    <div className="w-50 mx-auto">
+      <h2>Please log in</h2>
+      <form onSubmit={handleLogIn}>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
