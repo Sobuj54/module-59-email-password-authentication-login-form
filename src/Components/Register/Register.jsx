71ca9db85bae4c263f2ba6dec9f1066b42a70705
soubj <sobujahmed124@gmail.com>
2023-05-03 13:20:37 +0600

    created email password and submit form

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -4,6 +4,18 @@ const Register = () => {
   return (
     <div>
       <h2>Register</h2>
+      <form>
+        <input type="email" name="email" id="email" placeholder="email" />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
